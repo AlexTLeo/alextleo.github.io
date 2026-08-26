91ee726146670daafe31408698cd94c282a272f3
Alex T. Leonardi <leonardi.alex2@gmail.com>
2026-08-26 18:15:57 +0200

    Add .nojekyll to prevent GitHub from ignoring _astro folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://alextleo.github.io',
   base: '/',
+  outDir: './docs',
   trailingSlash: 'ignore',
   integrations: [mdx(), sitemap(), tailwind()]
 });
