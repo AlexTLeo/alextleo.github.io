1cc84bccacb802ab496b84caefc419b50dfe7744
Alex T. Leonardi <leonardi.alex2@gmail.com>
2026-08-30 02:38:36 +0200

    Update with real information. Remove irrelevant sections. Connect Contact form to Formspree. Set to Business theme.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Alex T. Leonardi'
+export const SITE_DESCRIPTION = 'Alex T. Leonardi is a skilled embedded systems engineer and robotics specialist with experience in developing deterministic C++ embedded drivers, advanced ROS/ROS2 robotics algorithms, and custom telemetry tools for autonomous fleets.'
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
